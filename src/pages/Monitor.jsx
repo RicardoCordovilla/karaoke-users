4d42626b0c4f69d5da547e17920c81239896ec2b
RicardoCordovilla <ricardocordovillaf@gmail.com>
2023-04-24 19:41:30 -0500

    fixing Monitor.jsx

--- src/pages/Monitor.jsx
+++ src/pages/Monitor.jsx
@@ -13,128 +13,128 @@ const socket = io('https://socketkaraoke-production.up.railway.app/')
 // console.log(users)
 
 function App() {
-  const [message, setMessage] = useState('')
-  const [displayMessage, setDisplayMessage] = useState()
-  const [mesas, setMesas] = useState([1, 2, 3, 4])
-  const [pedidos, setPedidos] = useState([{ name: '', pedido: '' }])
-  const [room, setRoom] = useState('')
-  const [users, setUsers] = useState(JSON.parse(localStorage.getItem('users')) || [])
-  // const [users, setUsers] = useState([])
+    const [message, setMessage] = useState('')
+    const [displayMessage, setDisplayMessage] = useState()
+    const [mesas, setMesas] = useState([1, 2, 3, 4])
+    const [pedidos, setPedidos] = useState([{ name: '', pedido: '' }])
+    const [room, setRoom] = useState('')
+    const [users, setUsers] = useState(JSON.parse(localStorage.getItem('users')) || [{ name: '', ci: '', pedidos: [''] }])
+    // const [users, setUsers] = useState([])
 
-  console.log(localStorage.getItem('users'))
-
-
-
-
-  const handleEnable = () => {
-    // socket.emit('enable', user, room)
-  }
-
-  const handleRoom = (user) => {
-    console.log(user)
-    socket.emit('join', user.ci)
-
-  }
-  const handleDisconnect = (user) => {
-    console.log(user)
-    socket.emit('djoin', user.ci)
-
-  }
-
-  const handleSend = (e) => {
-    e.preventDefault()
-    // console.log(message)
-    socket.emit('mensaje', message, room)
-  }
-
-  const handleErase = (user) => {
-    console.log(user)
-    socket.emit('erase', user.ci)
-    let arr = users.filter(item => item.ci !== user.ci)
-    console.log(arr)
-    localStorage.setItem('users', JSON.stringify(arr))
-    setUsers(arr)
-  }
-
-  useEffect(() => {
-
-    socket.on('connect', () => {
-      setDisplayMessage('Estás conectado con el id: ' + socket.id)
-    })
-
-    socket.on('add', (user) => {
-      console.log(user) 
-      console.log(users)
-      users.forEach(item => {
-        if (item.ci !== user?.ci) {
-          setUsers([...users, user])
-          localStorage.setItem('users', JSON.stringify([...users, user]))
-        }
-      })
-
-      if (users.length < 1) {
-        setUsers([...users, user])
-        localStorage.setItem('users', JSON.stringify([...users, user]))
-
-      }
-    })
-
-
-    socket.on('server-receive-message', (user) => {
-      console.log(user)
-      setDisplayMessage('mesa: ' + user?.mesa + ' dice: ' + JSON.stringify(user?.pedidos))
+    console.log(localStorage.getItem('users'))
 
 
 
-      let array = [user, ...users]
-      console.log(array)
-      let hash = {};
-      array = array.filter(o => hash[o?.ci] ? false : hash[o?.ci] = true)
-      console.log(array)
 
-      setUsers(array)
-      localStorage.setItem('users', JSON.stringify(array))
-
-      // if (message === 'disconnect') socket.disconnect()
-      // if (message === 'connect') socket.connect()
-    })
-
-    socket.on('connect_error', err => setDisplayMessage(err))
-
-    return () => {
-      socket.off('add')
+    const handleEnable = () => {
+        // socket.emit('enable', user, room)
     }
 
-  }, [users, displayMessage])
+    const handleRoom = (user) => {
+        console.log(user)
+        socket.emit('join', user.ci)
+
+    }
+    const handleDisconnect = (user) => {
+        console.log(user)
+        socket.emit('djoin', user.ci)
+
+    }
+
+    const handleSend = (e) => {
+        e.preventDefault()
+        // console.log(message)
+        socket.emit('mensaje', message, room)
+    }
+
+    const handleErase = (user) => {
+        console.log(user)
+        socket.emit('erase', user.ci)
+        let arr = users.filter(item => item.ci !== user.ci)
+        console.log(arr)
+        localStorage.setItem('users', JSON.stringify(arr))
+        setUsers(arr)
+    }
+
+    useEffect(() => {
+
+        socket.on('connect', () => {
+            setDisplayMessage('Estás conectado con el id: ' + socket.id)
+        })
+
+        socket.on('add', (user) => {
+            console.log(user)
+            console.log(users)
+            users.forEach(item => {
+                if (item.ci !== user?.ci) {
+                    setUsers([...users, user])
+                    localStorage.setItem('users', JSON.stringify([...users, user]))
+                }
+            })
+
+            if (users.length < 1) {
+                setUsers([...users, user])
+                localStorage.setItem('users', JSON.stringify([...users, user]))
+
+            }
+        })
+
+
+        socket.on('server-receive-message', (user) => {
+            console.log(user)
+            setDisplayMessage('mesa: ' + user?.mesa + ' dice: ' + JSON.stringify(user?.pedidos))
 
 
 
-  return (
-    <div className="App">
-      <motion.h3
-        initial={{ opacity: 0 }}
-        animate={{ opacity: 1 }}
-      >Monitor</motion.h3>
-      <motion.div className="mesasContainer">
+            let array = [user, ...users]
+            console.log(array)
+            let hash = {};
+            array = array.filter(o => hash[o?.ci] ? false : hash[o?.ci] = true)
+            console.log(array)
 
-        {
-          mesas.map((mesa, index) => (
-            <Mesa
-              key={index}
-              users={users}
-              mesa={mesa}
-              handleRoom={handleRoom}
-              handleDisconnect={handleDisconnect}
-              handleErase={handleErase}
-            />
-          ))
+            setUsers(array)
+            localStorage.setItem('users', JSON.stringify(array))
+
+            // if (message === 'disconnect') socket.disconnect()
+            // if (message === 'connect') socket.connect()
+        })
+
+        socket.on('connect_error', err => setDisplayMessage(err))
+
+        return () => {
+            socket.off('add')
         }
 
+    }, [users, displayMessage])
 
 
-      </motion.div>
-    </div>
-  )
+
+    return (
+        <div className="App">
+            <motion.h3
+                initial={{ opacity: 0 }}
+                animate={{ opacity: 1 }}
+            >Monitor</motion.h3>
+            <motion.div className="mesasContainer">
+
+                {
+                    mesas.map((mesa, index) => (
+                        <Mesa
+                            key={index}
+                            users={users}
+                            mesa={mesa}
+                            handleRoom={handleRoom}
+                            handleDisconnect={handleDisconnect}
+                            handleErase={handleErase}
+                        />
+                    ))
+                }
+
+
+
+            </motion.div>
+        </div>
+    )
 }
 
 export default App
