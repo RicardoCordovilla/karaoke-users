c4b9f59b4957dda3d7782220bdc7591a6d3b37f9
RicardoCordovilla <ricardocordovillaf@gmail.com>
2023-04-27 14:58:50 -0500

    flex wrap monitor mesas

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -2,7 +2,6 @@ import React from 'react'
 import { useNavigate, useParams } from 'react-router-dom'
 import FormRegister from '../components/FormRegister'
 import { useEffect } from 'react'
-import { v4 as uuid } from "uuid";
 import '../App.css'
 
 
@@ -10,8 +9,7 @@ const Login = () => {
     const params = useParams()
     const mesa = params.mesa
     console.log(mesa)
-    const id = uuid()
-    console.log(id)
+
 
     const navigate = useNavigate()
 
